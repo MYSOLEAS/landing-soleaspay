4e66c5f12ed3a5ea44df9bd8cdf6ea2038556052
Tchaya Yvan <tchayayvan76@gmail.com>
2023-08-13 19:56:37 +0200

    add visa card

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    // distDir: 'dist',
-    // basePath: '/home',
-    // reactStrictMode: true,
-    // images: {
-    //     //loader: "custom",
-    //     formats:['image/webp']
-    //   },
+    output: 'export',
+    distDir: 'dist',
+    basePath: '/home',
+    reactStrictMode: true,
+    images: {
+        //loader: "custom",
+        formats:['image/webp']
+      },
 }
 
 module.exports = nextConfig
